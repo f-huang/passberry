03ecf00da5f5dd88e3e0750302ba72a5d03d4841
Fanny Huang <>
2018-04-24 01:12:10 +0200

    react-server basic app file

---
+++ react-server/app.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// =====================================
+
+const express = require('express');
+const app = express();
+const router = require('./controllers/router');
+
+// =====================================
+
+const port = process.env.PORT || 3001;
+const middlewares = [
+	express.static(require('path').join(__dirname, '../')),
+	express.json(),
+	express.urlencoded()
+];
+const routes = [
+	'/',
+	'/sign-in'
+];
+
+// =====================================
+
+app.use(middlewares);
+app.use('/', router);
+app.use((req, res, next) => {
+	match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+		if (error)
+			res.status(500).send(error.message);
+		else if (redirectLocation)
+			res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+		else if (renderProps)
+			res.status(200);
+		else
+			res.status(404).send('Not found');
+	})
+});
+
+app.listen(port);
+
+console.log(`Listening app at http://localhost:${port}`);
+
+// =====================================
+
+module.exports = app;
