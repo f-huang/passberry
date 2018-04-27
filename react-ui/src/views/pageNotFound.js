e5961c42501a753106a1d3729d2ad6419d690297
Fanny Huang <>
2018-04-27 18:00:08 +0200

    Remove unused constructors

--- react-ui/src/views/pageNotFound.js
+++ react-ui/src/views/pageNotFound.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class PageNotFound extends Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<div className="PageNotFound">
