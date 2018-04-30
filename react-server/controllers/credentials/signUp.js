f325b198f26efe8f756ebc0941b9bcd9e01f0531
Fanny Huang <>
2018-04-30 22:59:44 +0200

    Update controller with promises

--- react-server/controllers/credentials/signUp.js
+++ react-server/controllers/credentials/signUp.js
@@ -2,19 +2,28 @@
 
 const router = require('express').Router();
 
-const addUser = require('../../models/User').addUser;
+const user = require('../../models/UserModel');
+const country = require('../../models/CountryModel');
+const address = require('../../models/AddressModel');
 
 router.post('/', (req, res) => {
-	const user = req.body;
-
-	addUser(user, (rows) => {
-		if (user) {
+	const form = req.body;
+	country.getCountryByName(form.country)
+		.then((code) => {
+			return {...form, ...{countryCode: code }};
+		})
+		.then((form) => {
+			address.addAddress(form)
+		})
+		.then((addressId) => {
+			form.addressId = addressId;
+			user.addUser(form);
+		})
+		.then((insertId) => {
 			res.send("OK");
-		}
-		else {
-			res.send("NO");
-
-		}
+		})
+	.catch(() => {
+		res.send("NO");
 	});
 });
 
