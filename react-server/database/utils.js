f3d47130d26c0d463b5eb4e27bb059352c13af8e
Fanny Huang <>
2018-04-30 22:57:58 +0200

    Put utils functions into a file

---
+++ react-server/database/utils.js
@@ -0,0 +1,35 @@
+"use strict";
+
+exports.getColumnsAliasesAsString = (columns, aliases) => {
+	let ret = "";
+
+	if (columns.length !== aliases.length)
+		throw "Columns and aliases not the same si´";
+	for (let i = 0; i < columns.length; i++) {
+		ret += `\`${columns[i]}\` AS '${aliases[i]}'` + (i + 1 === columns.length ? "" : ", ");
+	}
+	return ret;
+};
+
+exports.getColumnsAsString = (columns, usual) => {
+	let ret = "";
+
+	if (columns.length === 0 || typeof columns === undefined) {
+		columns = usual;
+	}
+	for (let i = 0; i < columns.length; i++) {
+		ret += `\`${columns[i]}\`` + (i + 1 === columns.length ? "" : ", ");
+	}
+	return ret;
+};
+
+
+exports.getValuesAsString = (columns)  => {
+	let str = "";
+	const count = (columns.match(/,/g) || []).length;
+	if (count === 0)
+		return str;
+	for (let i = 0; i < count + 1; i++)
+		str += (i === count) ? "?" : "?, ";
+	return (str);
+};
