c622fdbd2a47bbc855db5791cc3c099779029c7c
Fanny Huang <>
2018-05-02 02:30:21 +0200

    Add shop page

--- react-ui/src/views/Shop.js
+++ react-ui/src/views/Shop.js
@@ -1,10 +1,13 @@
 import React from "react";
+import BottomNavigationBar from "../component/BottomNavigationBar/BottomNavigationBar";
+import AppBar from "../component/AppBar/AppBar";
 
 class Shop extends React.Component {
 	render() {
 		return (
 			<div className="Shop">
-
+				<AppBar/>
+				<BottomNavigationBar/>
 			</div>
 		);
 	}
