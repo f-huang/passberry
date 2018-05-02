dbfd2839958c7719269ca44f60aa3893a3a9c3a2
Fanny Huang <>
2018-05-02 02:29:56 +0200

    Clean

--- react-ui/src/component/AppBar/AppBar.js
+++ react-ui/src/component/AppBar/AppBar.js
@@ -1,27 +1,11 @@
-import React from 'react'
-
-import themes from '../../app/themes.js';
-
-let styles = {
-    container: {
-        backgroundColor: themes.colorPrimary,
-        maxHeight: "104px",
-        padding: "1.5vh",
-        borderBottom: "2px solid #eeeeee"
-    },
-    title: {
-        margin: 0,
-        color: themes.colorInverse,
-        textAlign: 'center',
-        fontSize: '1.5em',
-    },
-};
+import React from "react";
+import "./AppBar.css";
 
 class AppBar extends React.Component {
     render() {
         return (
-            <div className="AppBar" style={styles.container}>
-                <h1 className="AppBar-title" style={styles.title}>{this.props.title}</h1>
+            <div className="AppBar">
+                <h1 className="AppBar-title">{this.props.title}</h1>
             </div>
         )
     }
@@ -31,4 +15,4 @@ AppBar.defaultProps = {
     title: "Title"
 };
 
-export default AppBar
+export default AppBar;
--- react-ui/src/component/BottomNavigationBar/BottomNavigationBar.js
+++ react-ui/src/component/BottomNavigationBar/BottomNavigationBar.js
@@ -1,52 +1,30 @@
-import React from 'react'
-import themes from '../../app/themes.js';
+import React from "react";
+import "./BottomNavigationBar.css";
 
-let items = {};
-let showTitle = false;
-let showIcons = true;
+import iconShop from "../../assets/search-empty.svg";
+import iconQR from "../../assets/qr-empty.svg";
+import iconPass from "../../assets/pass-empty.svg";
+import iconProfile from "../../assets/profile-empty.svg";
 
-let styles = {
-    container: {
-        width: "100vw",
-        height: "60px",
-        display: "flex",
-        justifyContent: "space-between",
-        position: "fixed",
-        textColor: themes.textColor,
-        fontSize: "1em",
-        bottom: 0,
-        borderTop: "2px solid #eeeeee"
-    },
-    item: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "0 12vw",
-        margin: "0 auto"
-    },
-    selectedItem: {
-        backgroundColor: "#f9f9f9"
-    },
-    image: {
-        height: "50%",
-        width: "auto"
-    },
-    title: {
-        margin: "0"
-    }
-};
+const items = [
+	{title: "Shop", alt: "shop-icon", icon: iconShop},
+	{title: "QR", alt: "qr-code-icon", icon: iconQR},
+	{title: "Pass", alt: "pass-icon", icon: iconPass},
+	{title: "Profile", alt: "profile-icon", icon: iconProfile},
+];
+
+const showTitle = false;
+const showIcons = true;
 
 class BottomNavigationItem extends React.Component {
     render() {
-        let divStyle = (this.props.isSelected === true) ? Object.assign({}, styles.item, styles.selectedItem) : styles.item;
         return (
-            <div className="BottomNavigationItem" style={divStyle}>
+            <div className="BottomNavigationItem">
                 {(showIcons) && <img className="BottomNavigationItem-icon"
                                      alt={this.props.item.alt}
                                      src={this.props.item.icon}
-                                     style={styles.image}/>}
-                {(showTitle) && <h6 className="BottomNavigationItem-title" style={styles.title}> {this.props.item.title}</h6>}
+                                    />}
+                {(showTitle) && <h6 className="BottomNavigationItem-title"> {this.props.item.title}</h6>}
             </div>
         )
     }
@@ -54,23 +32,13 @@ class BottomNavigationItem extends React.Component {
 
 
 class BottomNavigationBar extends React.Component {
-    constructor(props) {
-        super(props);
-        items = props.items;
-        if (props.showTitle)
-            showTitle = props.showTitle;
-        if (props.showIcons)
-            showIcons = props.showIcons;
-    }
-
     render() {
-        let selected = this.props.selectedItem;
         return (
-            <div className="BottomNavigationBar" style={styles.container}>
+            <div className="BottomNavigationBar">
                 {
                     items.map(function(item, i) {
-                        const isSelected = (item === selected);
-                        return <BottomNavigationItem item={item} key={i} isSelected={isSelected}/>
+                        // const isSelected = (item === selected);
+                        return <BottomNavigationItem item={item} key={i}/>
                     })
                 }
             </div>
@@ -78,4 +46,4 @@ class BottomNavigationBar extends React.Component {
     }
 }
 
-export default BottomNavigationBar
+export default BottomNavigationBar;
