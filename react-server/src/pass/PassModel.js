f6802dec7977df998a9425969628756d9788b73a
Fanny Huang <>
2018-09-21 20:18:44 +0200

    Pass resolver and model in progress

--- react-server/src/pass/PassModel.js
+++ react-server/src/pass/PassModel.js
@@ -25,3 +25,21 @@ exports.update = (pass) => new Promise((resolve, reject) => {
 		resolve(row.insertId);
 	});
 });
+
+exports.init = (pass) => new Promise((resolve, reject) => {
+	const sql = `
+		UPDATE ${TABLE_NAME}
+		SET
+		\`init_time\` = CASE WHEN \`init_time\` IS NULL THEN ? ELSE \`init_time\`, 
+		\`expiration_time\` = CASE WHEN \`expiration_time\` IS NULL THEN ? ELSE \`expiration_time\`
+		WHERE _id = ? 
+	`;
+	pool.query(sql, [pass.init_time, pass.expiration_time, pass._id], (error, result) => {
+		if (error) {
+			console.error(error);
+			reject(error);
+			return null;
+		}
+		resolve(result);
+	});
+});
---
+++ react-server/src/pass/PassResolver.js
@@ -0,0 +1,56 @@
+const Pass = require('./PassModel');
+const PassToAttraction = require('./PassToAttractionModel');
+const moment = require('moment');
+
+const EXPIRE_INS = '7';
+
+const resolver = {
+	Mutation: {
+		createTravelersPasses: (_, { input }) => {
+			const travelerIds = [...new Set(input.choices.map(choice => choice.user_id))];
+			const ret = [];
+			console.log("travelerIds :", travelerIds);
+			travelerIds.forEach(travelerId => {
+				const pass = {
+					"user_id": input.user_id,
+					"traveler_id": travelerId,
+					"basket_id": input._id,
+					// "start_date": startDate,
+					// "end_date": endDate
+				};
+				const userAttractions = input.choices.filter(choice => choice.user_id === travelerId);
+				console.log("userAttractions :", userAttractions);
+				Pass.create(pass).then(insertId => {
+					userAttractions.forEach(attraction => {
+							for (let i = 0; i < attraction.quantity; i++) {
+								PassToAttraction.create({
+									"pass_id": insertId,
+									"attraction_id": attraction._id
+								}).catch(e => console.error(e))
+							}
+						}
+					);
+					ret.push({passId: insertId , travelerId: travelerId});
+				}).catch(e => console.error(e));
+				return ret;
+			});
+		},
+		burnAttractionTicket: (_, { input }) => {
+			return PassToAttraction.update({
+				'_id': input.attraction_id,
+				'used_time': input.timestamp
+			}).then(result => {
+				Pass.update({
+					'_id': input.pass_id,
+					'init_time': input.timestamp,
+					'expiration_time': moment(input.timestamp).add(EXPIRE_INS, 'days')
+				}).then(result => {
+					return ({code: 0, message: 'OK'});
+				})
+			})
+
+		},
+	}
+};
+
+module.exports = resolver;
