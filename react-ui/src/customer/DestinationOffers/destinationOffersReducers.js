6850bf505bfc18f44cd46142dcf2f604e8e841c3
Fanny Huang <>
2018-09-28 09:55:17 +0200

    Fixing basket reducer : 	- Basket.items = { travelerId, quantity, product: {} } --> item renamed to product to avoid basket.item.item.id --> now : basket.item.product.id 	- Updating quantity depending on travelerId and product.id instead of justing adding item

--- react-ui/src/customer/DestinationOffers/destinationOffersReducers.js
+++ react-ui/src/customer/DestinationOffers/destinationOffersReducers.js
@@ -7,9 +7,10 @@ import {
 } from "./destinationOffersActions";
 import { BASKET } from "../localStorageKeys";
 import { createApolloFetch } from 'apollo-fetch';
+import moment from "moment";
 
 let basket = localStorage.getItem(BASKET);
-basket = basket ? JSON.parse(basket) : [];
+basket = basket ? JSON.parse(basket) : {};
 
 const fetch = createApolloFetch();
 // let attractions = apiCall(Attra)
@@ -37,16 +38,38 @@ function basketReducer(state = basket, action) {
 	let newBasket;
 	switch (action.type) {
 		case ADD_TO_BASKET:
-			newBasket = [...state, action.item];
+			const lastUpdateTime = Date.now();
+			const initTime = state.length > 0 ? state.initTime : lastUpdateTime;
+			const indexItem = state.items ? state.items.findIndex(basketItem =>
+				basketItem.travelerId === action.item.travelerId && basketItem.product.id === action.item.product.id
+			) : -1;
+			let newItems;
+			if (state.items === undefined || !state.items || !state.items.length)
+				newItems = [action.item];
+			else if (indexItem !== -1) {
+				newItems = state.items;
+				newItems[indexItem].quantity = state.items[indexItem].quantity + 1;
+			}
+			else {
+				newItems = [...state.items, action.item];
+			}
+			newBasket = {
+				lastUpdateTime,
+				initTime,
+				items: newItems
+			};
 			localStorage.setItem(BASKET, JSON.stringify(newBasket));
 			return newBasket;
+
 		case REMOVE_FROM_BASKET:
-			const index = state.findIndex(item =>
-				item.attractionId === action.item.id && item.travelerId === action.item.travelerId);
-			if (index >= 0 && index < state.length) {
-				newBasket = state.slide(0);
-				if (newBasket[index].quantity > 0)
-					newBasket[index].quantity--;
+			const index = state.items.findIndex(basketItem =>
+				basketItem.travelerId === action.item.travelerId && basketItem.product.id === action.item.product.id);
+			if (index >= 0 && index < state.items.length) {
+				const newItems = state.items;
+				if (newItems[index].quantity > 0)
+					newItems[index].quantity = newItems[index].quantity - 1;
+				newBasket = {...state, items: newItems};
+				localStorage.setItem(BASKET, JSON.stringify(newBasket));
 				return newBasket;
 			}
 			else
