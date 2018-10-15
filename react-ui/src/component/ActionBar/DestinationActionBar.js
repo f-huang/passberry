62a87fb7b13759aff3baa7072130801a1a74b8d8
Fanny Huang <>
2018-10-15 14:39:39 +0200

    Renaming ActionBar to DestinationActionBar -> each view with one actionbar ?

---
+++ react-ui/src/component/ActionBar/DestinationActionBar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import backIcon from "../../assets/icons/arrow_left_white.svg";
+import theme from "../../app/theme";
+import { NavLink } from "react-router-dom";
+
+const Bar = styled.div`
+	height: 7vh;
+	width: 100%;
+	position: relative;
+	background-color: ${theme.colorYellow};
+`;
+
+const Container = styled.div`
+	position: relative;
+	display: flex;
+	align-items: center;
+	padding: 12px;
+`;
+
+const Icon = styled.img`
+	width: 20px;
+	height: 20px;
+`;
+
+const Title = styled.h3`
+	font-size: 18px;
+	color: ${theme.colorInverse};
+	margin: 0;
+	padding: 0 8px;
+`;
+
+const DestinationActionBar = (props) =>
+	<Bar>
+		<Container>
+			<NavLink to={props.to}>
+				<Icon src={backIcon} alt="back-arrow"/>
+			</NavLink>
+			<Title>{ props.title }</Title>
+		</Container>
+	</Bar>;
+
+
+DestinationActionBar.propTypes = {
+	to: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+};
+
+export default DestinationActionBar;
